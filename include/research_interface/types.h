f783cafdb5b7ce7d74f916f2b5d89cd03949c2c1
Lukasz Duda <lukasz.duda@franka.de>
2017-02-15 15:18:39 +0100

    Start and stop motion generators and the controller

--- include/research_interface/types.h
+++ include/research_interface/types.h
@@ -9,7 +9,13 @@ using Version = uint16_t;
 constexpr Version kVersion = 1;
 constexpr uint16_t kCommandPort = 1337;
 
-enum class Function : uint32_t { kConnect = 0 };
+enum class Function : uint32_t {
+  kConnect = 0,
+  kStartMotionGenerator = 1,
+  kStopMotionGenerator = 2,
+  kStartController = 3,
+  kStopController = 4
+};
 
 struct ConnectRequest {
   ConnectRequest(uint16_t udp_port)
@@ -30,6 +36,75 @@ struct ConnectReply {
 
   const Status status;
   const Version version;
+};
+
+struct StartMotionGeneratorRequest {
+  enum class Type : uint32_t {
+    kJointPosition = 0,
+    kJointVelocity = 1,
+    kCartesianPosition = 2,
+    kCartesianVelocity = 3
+  };
+
+  StartMotionGeneratorRequest(Type type)
+      : function(Function::kStartMotionGenerator), type(type) {}
+
+  const Function function;
+  const Type type;
+};
+
+struct StartMotionGeneratorReply {
+  enum class Status : uint32_t {
+    kSuccess = 0,
+    kNotConnected = 1,
+    kInvalidType = 2
+  };
+
+  StartMotionGeneratorReply(Status status) : status(status) {}
+  const Status status;
+};
+
+struct BaseModeChangeReply {
+  enum class Status : uint32_t {
+    kSuccess = 0,
+    kNotConnected = 1
+  };
+
+  BaseModeChangeReply(Status status) : status(status) {}
+  const Status status;
+};
+
+struct StopMotionGeneratorRequest {
+  StopMotionGeneratorRequest()
+      : function(Function::kStopMotionGenerator) {}
+
+  const Function function;
+};
+
+struct StopMotionGeneratorReply : public BaseModeChangeReply {
+  using BaseModeChangeReply::BaseModeChangeReply;
+};
+
+struct StartControllerRequest {
+  StartControllerRequest()
+      : function(Function::kStartController) {}
+
+  const Function function;
+};
+
+struct StartControllerReply : public BaseModeChangeReply {
+  using BaseModeChangeReply::BaseModeChangeReply;
+};
+
+struct StopControllerRequest {
+  StopControllerRequest()
+      : function(Function::kStopController) {}
+
+  const Function function;
+};
+
+struct StopControllerReply : public BaseModeChangeReply {
+  using BaseModeChangeReply::BaseModeChangeReply;
 };
 
 }  // namespace research_interface
