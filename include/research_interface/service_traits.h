d77a0d1537044dadf9d4a77fc964344b92420f68
Simon Gabl <simon.gabl@franka.de>
2017-05-16 19:42:44 +0200

    Added Command Traits

---
+++ include/research_interface/service_traits.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "service_types.h"
+
+namespace research_interface {
+
+template<typename T>
+struct CommandTraits {};
+
+template<>
+struct CommandTraits<StartMotionGenerator> {
+  static constexpr const char* kName = "Start Motion Generator";
+};
+
+template<>
+struct CommandTraits<StopMotionGenerator> {
+  static constexpr const char* kName = "Stop Motion Generator";
+};
+
+template<>
+struct CommandTraits<StartController> {
+  static constexpr const char* kName = "Start Controller";
+};
+
+template<>
+struct CommandTraits<StopController> {
+  static constexpr const char* kName = "Stop Controller";
+};
+
+template<>
+struct CommandTraits<GetCartesianLimit> {
+  static constexpr const char* kName = "Get Cartesian Limit";
+};
+
+template<>
+struct CommandTraits<SetControllerMode> {
+  static constexpr const char* kName = "Set Controller Mode";
+};
+
+template<>
+struct CommandTraits<SetCollisionBehavior> {
+  static constexpr const char* kName = "Set Collision Behavior";
+};
+
+template<>
+struct CommandTraits<SetJointImpedance> {
+  static constexpr const char* kName = "Set Joint Impedance";
+};
+
+template<>
+struct CommandTraits<SetCartesianImpedance> {
+  static constexpr const char* kName = "Set Cartesian Impedance";
+};
+
+template<>
+struct CommandTraits<SetGuidingMode> {
+  static constexpr const char* kName = "Set Guiding Mode";
+};
+
+template<>
+struct CommandTraits<SetEEToK> {
+  static constexpr const char* kName = "Set EE to K";
+};
+
+template<>
+struct CommandTraits<SetFToEE> {
+  static constexpr const char* kName = "Set F to EE";
+};
+
+template<>
+struct CommandTraits<SetLoad> {
+  static constexpr const char* kName = "Set Load";
+};
+
+template<>
+struct CommandTraits<SetTimeScalingFactor> {
+  static constexpr const char* kName = "Set Time Scaling Factor";
+};
+
+template<>
+struct CommandTraits<AutomaticErrorRecovery> {
+  static constexpr const char* kName = "Automatic Error Recovery";
+};
+
+template<>
+struct CommandTraits<ResetExternalTorqueAndForceMax> {
+  static constexpr const char* kName = "Reset External Torque and Force Max";
+};
+
+}  // namespace research_interface
