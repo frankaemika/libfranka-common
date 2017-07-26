6cccda21125ce9b31e31ce55c728ec78804a9691
Florian Walch <florian.walch@franka.de>
2017-07-26 17:50:48 +0200

    Add command IDs for requests and responses.

--- include/research_interface/gripper/types.h
+++ include/research_interface/gripper/types.h
@@ -13,19 +13,28 @@ using Version = uint16_t;
 constexpr Version kVersion = 1;
 constexpr uint16_t kCommandPort = 1338;
 
-enum class Function : uint16_t { kConnect, kHoming, kGrasp, kMove, kStop };
+enum class Command : uint16_t { kConnect, kHoming, kGrasp, kMove, kStop };
+
+struct CommandHeader {
+  CommandHeader() = default;
+  CommandHeader(Command command, uint32_t command_id) : command(command), command_id(command_id) {}
+
+  Command command;
+  uint32_t command_id;
+};
 
 template <typename T>
 struct RequestBase {
-  RequestBase() : function(T::kFunction) {}
-  const Function function;
+  RequestBase(uint32_t command_id) : header(T::kCommand, command_id) {}
+  const union { const CommandHeader header; };
 };
 
 template <typename T>
 struct ResponseBase {
-  ResponseBase(typename T::Status status) : function(T::kFunction), status(status) {}
+  ResponseBase(uint32_t command_id, typename T::Status status)
+      : header(T::kCommand, command_id), status(status) {}
 
-  const Function function;
+  const union { const CommandHeader header; };
   const typename T::Status status;
 
   static_assert(std::is_enum<decltype(status)>::value, "Status must be an enum.");
@@ -36,38 +45,42 @@ struct ResponseBase {
                 "Status must define kSuccess with value of 0.");
 };
 
-template <typename T, Function F>
+template <typename T, Command C>
 struct CommandBase {
-  static constexpr Function kFunction = F;
+  static constexpr Command kCommand = C;
 
   enum class Status : uint16_t { kSuccess, kFail, kUnsuccessful };
 
+  using Header = CommandHeader;
   using Request = RequestBase<T>;
   using Response = ResponseBase<T>;
 };
 
-struct Connect : CommandBase<Connect, Function::kConnect> {
+struct Connect : CommandBase<Connect, Command::kConnect> {
   enum class Status : uint16_t { kSuccess, kIncompatibleLibraryVersion };
 
   struct Request : public RequestBase<Connect> {
-    Request(uint16_t udp_port) : version(kVersion), udp_port(udp_port) {}
+    Request(uint32_t command_id, uint16_t udp_port)
+        : RequestBase(command_id), version(kVersion), udp_port(udp_port) {}
 
     const Version version;
     const uint16_t udp_port;
   };
 
   struct Response : public ResponseBase<Connect> {
-    Response(Status status) : ResponseBase(status), version(kVersion) {}
+    Response(uint32_t command_id, Status status)
+        : ResponseBase(command_id, status), version(kVersion) {}
 
     const Version version;
   };
 };
 
-struct Homing : public CommandBase<Homing, Function::kHoming> {};
+struct Homing : public CommandBase<Homing, Command::kHoming> {};
 
-struct Grasp : public CommandBase<Grasp, Function::kGrasp> {
+struct Grasp : public CommandBase<Grasp, Command::kGrasp> {
   struct Request : public RequestBase<Grasp> {
-    Request(double width, double speed, double force) : width(width), speed(speed), force(force) {}
+    Request(uint32_t command_id, double width, double speed, double force)
+        : RequestBase(command_id), width(width), speed(speed), force(force) {}
 
     const double width;
     const double speed;
@@ -75,16 +88,17 @@ struct Grasp : public CommandBase<Grasp, Function::kGrasp> {
   };
 };
 
-struct Move : public CommandBase<Move, Function::kMove> {
+struct Move : public CommandBase<Move, Command::kMove> {
   struct Request : public RequestBase<Move> {
-    Request(double width, double speed) : width(width), speed(speed) {}
+    Request(uint32_t command_id, double width, double speed)
+        : RequestBase(command_id), width(width), speed(speed) {}
 
     const double width;
     const double speed;
   };
 };
 
-struct Stop : public CommandBase<Stop, Function::kStop> {};
+struct Stop : public CommandBase<Stop, Command::kStop> {};
 
 struct GripperState {
   uint32_t message_id;
